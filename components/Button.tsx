5504335ec9eead582ec3fc72d7570b0c65957531
Ekku Lehto <ekkulehto@proton.me>
2025-10-15 15:17:22 +0300

    fix: Fix label not showing right

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,7 +8,7 @@ export default function Button({label}: Props) {
     return (
         <View style={styles.buttonContainer}>
             <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
-                <Text style={styles.buttonLabel}>(label)</Text>
+                <Text style={styles.buttonLabel}>{label}</Text>
             </Pressable>
         </View>
     );
