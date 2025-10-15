52ce173ef62c3b3a017eaf037750b73217feaab6
Ekku Lehto <ekkulehto@proton.me>
2025-10-15 16:10:55 +0300

    feat: Display placeholder- or user chosen image

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -2,12 +2,15 @@ import { Image } from "expo-image";
 import { ImageSourcePropType, StyleSheet } from "react-native";
 
 type Props = {
-    imgSource: ImageSourcePropType
+    imgSource: ImageSourcePropType;
+    selectedImage?: string;
 }
 
-export default function ImageViewer({imgSource}: Props) {
+export default function ImageViewer({imgSource, selectedImage}: Props) {
+  const imageSource = selectedImage ? {uri: selectedImage} : imgSource;
+
   return (
-        <Image source={imgSource} style={styles.image} />
+        <Image source={imageSource} style={styles.image} />
   );
 }
 
