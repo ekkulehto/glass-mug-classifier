46862b826e202dbf1e7544e77105920282160dd0
Ekku Lehto <ekkulehto@proton.me>
2025-10-15 17:22:48 +0300

    feat: Add buttons with options for url, camera roll, camera (work in progress)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,12 +3,15 @@ import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 
 import Button from "@/components/Button";
+import CircleButton from '@/components/CircleButton';
+import IconButton from '@/components/IconButton';
 import ImageViewer from '@/components/ImageViewer';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -19,20 +22,46 @@ export default function Index() {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri)
-    } else {
-      alert('You did not select any image.')
+      setShowAppOptions(true);
     }
   }
+
+  const takeImageAsync = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    })
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri)
+      setShowAppOptions(true);
+    }
+  }
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.ImageContainer}>
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
-      <View style={styles.footerContainer}>
-        <Button theme="primary" label='Choose a photo' onPress={pickImageAsync} />
-        <Button label='Use this photo' />
-      </View>
+      {showAppOptions ? (
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label='Use this photo' onPress={pickImageAsync} />
+          <Button label='Choose another photo' onPress={() => setShowAppOptions(false)} />
+        </View>
+      ) : (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon='link' label='Url' onPress={onReset} />
+            <CircleButton onPress={pickImageAsync} />
+            <IconButton icon='camera-alt' label='Photo' onPress={takeImageAsync} />
+          </View>
+        </View>
+      )}
     </View>
   );
 }
@@ -47,7 +76,15 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   footerContainer: {
-    flex: 1/3,
+    flex: 1 / 3,
     alignItems: 'center',
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   }
 })
