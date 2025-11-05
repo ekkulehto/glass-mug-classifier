4dd88b6f5a52c9a1cf9d37fa0437d00d62d0ac78
Ekku Lehto <ekkulehto@proton.me>
2025-11-05 14:11:53 +0200

    feat: Add RNR sign-in-form

---
+++ components/sign-in-form.tsx
@@ -0,0 +1,35 @@
+import { SocialConnections } from '@/components/social-connections';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
+import { Text } from '@/components/ui/text';
+import * as React from 'react';
+import { View } from 'react-native';
+
+export function SignInForm() {
+  return (
+    <View className="gap-6">
+      <Card className="border-border/0 sm:border-border shadow-none sm:shadow-sm sm:shadow-black/5">
+        <CardHeader>
+          <CardTitle className="text-center text-xl sm:text-left">Sign in to Glass-Mug-Classifier</CardTitle>
+          <CardDescription className="text-center sm:text-left">
+            Welcome! Please sign in to continue
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="gap-6">
+          <View className="flex-row items-center">
+            <Separator className="flex-1" />
+            <Text className="text-muted-foreground px-4 text-sm">social</Text>
+            <Separator className="flex-1" />
+          </View>
+          <SocialConnections />
+        </CardContent>
+      </Card>
+    </View>
+  );
+}
