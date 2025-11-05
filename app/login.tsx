28f81cc2dee4f7945de87d44379fbb601c1e2534
Ekku Lehto <ekkulehto@proton.me>
2025-11-05 14:11:20 +0200

    feat: Replace login screen with RNR-component

--- app/login.tsx
+++ app/login.tsx
@@ -1,67 +1,15 @@
-import { useAuth } from "@/context/AuthContext";
-import { exchangeCodeForToken, useAuthRequest } from "@/lib/auth";
-import * as WebBrowser from 'expo-web-browser';
-import { useEffect } from "react";
-import { ActivityIndicator, Button, StyleSheet, Text, View } from "react-native";
-
-WebBrowser.maybeCompleteAuthSession();
-
-export default function LoginScreen() {
-  const { setSession } = useAuth();
-  const [request, response, promptAsync] = useAuthRequest();
-
-  useEffect(() => {
-    if (response) {
-      if (response.type === 'success' && request?.codeVerifier) {
-        const { code } = response.params;
-        const redirectUri = request.redirectUri;
-        exchangeCodeForToken(code, request.codeVerifier, redirectUri).then(newSession => {
-          setSession(newSession);
-        });
-      } else if (response.type === 'error') {
-        console.error('Authentication error:', response.error);
-      }
-    }
-  }, [response, request]);
-
-  if (!request) {
-    return (
-        <View style={styles.container}>
-            <ActivityIndicator size="large" color="#ffd33d" />
-        </View>
-    );
-  }
-
+import { SignInForm } from '@/components/sign-in-form';
+import { ScrollView, View } from 'react-native';
+ 
+export default function SignInScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome</Text>
-      <Text style={styles.subtitle}>Please sign in to continue</Text>
-      <Button
-        title="Sign in with Microsoft"
-        onPress={() => promptAsync()}
-        disabled={!request}
-      />
-    </View>
+    <ScrollView
+      keyboardShouldPersistTaps="handled"
+      contentContainerClassName="sm:flex-1 items-center justify-center p-4 py-8 sm:py-4 sm:p-6 mt-safe"
+      keyboardDismissMode="interactive">
+      <View className="w-full max-w-sm">
+        <SignInForm />
+      </View>
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#25292e',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 20,
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: '#fff',
-        marginBottom: 8,
-    },
-    subtitle: {
-        fontSize: 16,
-        color: '#ccc',
-        marginBottom: 24,
-    }
-});
