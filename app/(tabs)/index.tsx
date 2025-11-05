a1ed6f72e2831095c40417f04f37e359c8ce369c
Ekku Lehto <ekkulehto@proton.me>
2025-11-05 17:18:59 +0200

    feat: Use RNR class names for styling and change urlimagepicker-modal to RNR-dialog

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from "react";
-import { ActivityIndicator, Alert, StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, Alert, Text, View } from "react-native";
 
 import { predictFromFile, predictFromUrl, type CvPrediction } from '@/api/customVision';
 import Button from "@/components/Button";
@@ -8,7 +8,8 @@ import CircleButton from '@/components/CircleButton';
 import IconButton from '@/components/IconButton';
 import ImageViewer from '@/components/ImageViewer';
 import Predictions from '@/components/Predictions';
-import UrlImagePicker from '@/components/UrlImagePicker';
+import UrlButton from '@/components/UrlButton';
+import { useColorScheme } from 'nativewind';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -17,10 +18,10 @@ type SourceKind = 'none' | 'url' | 'file';
 export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
-  const [isUrlImagePickerVisible, setisUrlImagePickerVisible] = useState<boolean>(false);
   const [sourceType, setSourceType] = useState<SourceKind>('none');
   const [preds, setPreds] = useState<CvPrediction[] | null>(null)
   const [loading, setLoading] = useState<boolean>(false)
+  const {colorScheme} = useColorScheme()
 
   const useThisPhoto = async () => {
     if (!selectedImage || sourceType === 'none') {
@@ -74,7 +75,6 @@ export default function Index() {
     setSelectedImage(uri);
     setSourceType('url');
     setShowAppOptions(true);
-    setisUrlImagePickerVisible(false);
   };
 
   const onReset = () => {
@@ -84,30 +84,22 @@ export default function Index() {
     setPreds(null);
   };
 
-  const onModalClose = () => {
-    setisUrlImagePickerVisible(false)
-  }
-
-  const onAddurl = () => {
-    setisUrlImagePickerVisible(true)
-  }
-
   return (
-    <View style={styles.container}>
-      <View style={styles.ImageContainer}>
+    <View className='flex-1 mx-auto items-center'>
+      <View>
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
         {loading ? (
-          <View style={styles.loadingCard}>
-            <ActivityIndicator size={'large'} color={'#ffd33d'} />
-            <Text style={styles.loadingText}>Loading...</Text>
+          <View className='flex-1 mx-auto justify-center'>
+            <ActivityIndicator size={'large'} color={colorScheme === 'dark' ? 'white' : 'black'} />
+            <Text>Loading...</Text>
           </View>
         ) : (
           preds && <Predictions data={preds} />
         )
         }
-      </View>
+      </View >
       {showAppOptions ? (
-        <View style={styles.footerContainer}>
+        <View className='flex-1 mx-auto justify-center'>
           {preds === null && !loading && (
             <Button theme="primary" label='Use this photo' onPress={useThisPhoto} />
           )
@@ -118,52 +110,12 @@ export default function Index() {
           }
         </View>
       ) : (
-        <View style={styles.optionsContainer}>
-          <View style={styles.optionsRow}>
-            <IconButton icon='link' label='Url' onPress={onAddurl} />
+        <View className='flex-1 flex-row mx-auto items-center'>
+            <UrlButton onConfirm={handleUrlPicked} />
             <CircleButton onPress={pickImageAsync} />
             <IconButton icon='camera-alt' label='Photo' onPress={takeImageAsync} />
-          </View>
         </View>
       )}
-      <UrlImagePicker isVisible={isUrlImagePickerVisible} onClose={onModalClose} onConfirm={handleUrlPicked} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-  },
-  ImageContainer: {
-    flex: 1,
-  },
-  footerContainer: {
-    flex: 1 / 3,
-    alignItems: 'center',
-  },
-  optionsContainer: {
-    position: 'absolute',
-    bottom: 80,
-  },
-  optionsRow: {
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  loadingCard: {
-    alignSelf: 'center',
-    minWidth: 260,
-    padding: 12,
-    borderRadius: 10,
-    marginTop: 20,
-  },
-  loadingText: {
-    color: '#fff',
-    fontWeight: '700',
-    fontSize: 16,
-    textAlign: 'center',
-    marginTop: 10,
-  }
-})
