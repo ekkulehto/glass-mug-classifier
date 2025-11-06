9ed6067135aef7040389b618fc0f06fc86c8607a
Ekku Lehto <ekkulehto@proton.me>
2025-11-06 17:34:34 +0200

    feat: Center the login-screen

--- app/login.tsx
+++ app/login.tsx
@@ -5,7 +5,7 @@ export default function SignInScreen() {
   return (
     <ScrollView
       keyboardShouldPersistTaps="handled"
-      contentContainerClassName="sm:flex-1 items-center justify-center p-4 py-8 sm:py-4 sm:p-6 mt-safe"
+      contentContainerClassName="flex-1 items-center justify-center p-4 py-8 sm:py-4 sm:p-6 mt-safe"
       keyboardDismissMode="interactive">
       <View className="w-full max-w-sm">
         <SignInForm />
