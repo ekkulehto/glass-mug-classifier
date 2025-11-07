fe59cedaf93ea6b7ab2a55a5cda560cf74c51278
Ekku Lehto <ekkulehto@proton.me>
2025-11-07 16:57:35 +0200

    feat: Style iconbuttons to use lucide icons

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -1,29 +1,21 @@
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { Pressable, StyleSheet } from "react-native";
-import { Text } from './ui/text';
+import { Button } from "./ui/button";
+import { Text } from "./ui/text";
+import { useTheme } from "@react-navigation/native";
+import type { LucideIcon } from "lucide-react-native";
 
 type Props = {
-    icon: keyof typeof MaterialIcons.glyphMap;
-    label: string,
-    onPress: () => void;
+  icon: LucideIcon;          // esim. Camera, User, Settings...
+  label: string;
+  onPress?: () => void;
 };
 
-export default function IconButton({icon, label, onPress}: Props) {
-    return (
-        <Pressable style={styles.iconButton} onPress={onPress}>
-            <MaterialIcons name={icon} size={24} color='#fff'/>
-            <Text style={styles.iconButtonLabel}>{label}</Text>
-        </Pressable>
-    )
-}
+export default function IconButton({ icon: IconComp, label, onPress }: Props) {
+  const { colors } = useTheme();
 
-const styles = StyleSheet.create({
-    iconButton: {
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    iconButtonLabel: {
-        color: '#fff',
-        marginTop: 12,
-    },
-})
+  return (
+    <Button onPress={onPress} className="flex-col items-center justify-center" style={{backgroundColor: colors.background}}>
+      <IconComp size={24} strokeWidth={1.3} color={colors?.primary ?? "#000"} />
+      <Text className="mt-2" style={{color: colors.primary}}>{label}</Text>
+    </Button>
+  );
+}
