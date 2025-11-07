70a1dae13814b4e2bec414ab8b78872643628dc9
Ekku Lehto <ekkulehto@proton.me>
2025-11-07 16:59:43 +0200

    feat: Add HelpGuideProvider dialog and update icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,50 +3,83 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import { useTheme } from '@react-navigation/native';
 import { Tabs } from "expo-router";
 import { View } from 'react-native';
+import { useHelpGuide } from '@/components/HelpGuide';
+import { HelpGuideProvider } from "@/components/HelpGuide";
+import { Button } from '@/components/ui/button';
+import { Icon } from '@/components/ui/icon';
+import { CircleQuestionMarkIcon } from 'lucide-react-native';
+
+function HeaderHelpIconButton() {
+    const { colors } = useTheme();
+    const { open } = useHelpGuide();
+
+    return (
+        <Button
+            size="icon"
+            className="size-10 rounded-full"
+            onPress={open}
+            style={{backgroundColor: colors.background}}
+        >
+            <Icon
+                as={CircleQuestionMarkIcon}
+                className='w-full'
+                strokeWidth={1.1}
+                color={colors.primary}
+            />
+        </Button>
+    );
+}
 
 export default function TabLayout() {
     const { colors } = useTheme();
 
     return (
-        <Tabs
-            screenOptions={{
-                tabBarActiveTintColor: colors.primary,
-                headerStyle: {
-                    backgroundColor: colors.background,
-                },
-                headerShadowVisible: false,
-                headerTintColor: '#fff',
-                tabBarStyle: {
-                    backgroundColor: colors.background
-                },
-                headerRight: () => (
-                    <View className='mr-5'>
-                        <UserMenu />
-                    </View>
-                )
-            }}
-        >
-            <Tabs.Screen
-                name='index'
-                options={{
-                    title: 'Home',
-                    headerTitleAlign: 'center',
-                    tabBarIcon: ({ color, focused }) => (
-                        <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+        <HelpGuideProvider>
+            <Tabs
+                screenOptions={{
+                    tabBarActiveTintColor: colors.primary,
+                    headerStyle: {
+                        backgroundColor: colors.background,
+                    },
+                    headerShadowVisible: false,
+                    headerTintColor: '#fff',
+                    tabBarStyle: {
+                        backgroundColor: colors.background
+                    },
+                    headerRight: () => (
+                        <View className='mr-5'>
+                            <UserMenu />
+                        </View>
                     ),
+                    headerLeft: () => (
+                        <View className='ml-5'>
+                            <HeaderHelpIconButton />
+                        </View>
+                    )
+                }}
+            >
+                <Tabs.Screen
+                    name='index'
+                    options={{
+                        title: 'Home',
+                        headerTitleAlign: 'center',
+                        tabBarIcon: ({ color, focused }) => (
+                            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+                        ),
 
-                }}
-            />
-            <Tabs.Screen
-                name='about'
-                options={{
-                    title: 'About',
-                    headerTitleAlign: 'center',
-                    tabBarIcon: ({ color, focused }) => (
-                        <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
-                    ),
-                }}
-            />
-        </Tabs>
+                    }}
+                />
+                <Tabs.Screen
+                    name='about'
+                    options={{
+                        title: 'About',
+                        headerTitleAlign: 'center',
+                        tabBarIcon: ({ color, focused }) => (
+                            <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
+                        ),
+                    }}
+                />
+            </Tabs>
+        </HelpGuideProvider>
     )
 }
