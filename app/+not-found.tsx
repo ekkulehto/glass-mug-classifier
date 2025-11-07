dc93a6b198e54a53cea321578216ccfc84b8fa7d
Ekku Lehto <ekkulehto@proton.me>
2025-11-07 16:56:07 +0200

    feat: Show login toast

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -2,10 +2,21 @@ import { Stack } from "expo-router";
 import { View} from "react-native";
 import { useTheme } from "@react-navigation/native";
 import { Text } from '@/components/ui/text';
+import { showLoginSuccessToast } from "@/lib/toasts";
+import { useRef, useEffect } from "react";
 
 export default function AuthCallback() {
   const { colors } = useTheme();
-
+  
+    const fired = useRef(false);
+  
+    useEffect(() => {
+      if (fired.current) return;
+      fired.current = true;
+  
+      showLoginSuccessToast();
+    }, []);
+  
   return (
     <>
       <Stack.Screen options={{ headerShown: false }} />
